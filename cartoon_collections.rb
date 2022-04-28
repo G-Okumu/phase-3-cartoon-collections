d442403e126b7aca5132f3a346377096473de235
George-Okumu <gokumu12@gmail.com>
2022-04-29 01:19:38 +0300

    assignment complete

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+def roll_call_dwarves names
+  names.map.with_index(1) do |name, name_index|
+    puts "#{name_index}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet planeteer_calls
+  planeteer_calls.map do |planeteer|
+    planeteer.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls calls
+  calls.any? {|call| call.size > 4}
 end
 
-def find_the_cheese# code an argument here
+
+
+def find_the_cheese array_str
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array_str.find do |str|
+    cheese_types.include? str
+  end
 end
+
+binding.pry
+
